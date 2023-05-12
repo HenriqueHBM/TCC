1e564a165da42e95bae1e90dbec0d42d7d6a0ffb
HenriqueHBM <henriquemadeira1@gmail.com>
2023-05-12 12:49:02 -0300

    comeco do projeto

---
+++ example-app/resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <!-- Icone na Barra de Pesquisa -->
+    <link rel="shortcut icon"  href="./img/icon.webp">
+    <title>@yield('pagetitle',"Welcome to the Mato")</title>
+
+    <link rel="stylesheet" href="{{ asset('site/style.css') }}">
+
+    
+</head>
+<body>
+    <h1>Testando</h1>
+    <button class="btn btn-lg btn-orange">sdfsdf</button>
+
+    <script src="{{ asset('site/jquery.js') }}"></script>
+    <script src="{{ asset('site/bootstrap.js') }}"></script>
+</body>
+</html>
