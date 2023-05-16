da6ade62abc5882e912e6f7a289886dc1bb6af2d
HenriqueHBM <henriquemadeira1@gmail.com>
2023-05-16 12:43:50 -0300

    puts

--- example-app/resources/views/home.blade.php
+++ example-app/resources/views/home.blade.php
@@ -1,19 +1,5 @@
 @extends("layouts.app")
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <!-- Icone na Barra de Pesquisa -->
-    <link rel="shortcut icon"  href="./img/icon.webp">
-    <title> @yield('title'," Welcome to the Mato ")</title>
-    
-    
+@section('title', 'Home')
+@section('content')
 
-    
-</head>
-<body>
-    <h1>Pagina Inicial</h1>
-    <button id="ola" class="btn btn-lg btn-orange">O botão</button>
-
-</body>
-</html>
+@endsection
--- example-app/resources/views/layouts/app.blade.php
+++ example-app/resources/views/layouts/app.blade.php
@@ -2,22 +2,30 @@
 <html lang="pt-br">
 
 <head>
+    {{-- titulo da pag --}}
+    <title>@yield('title')</title>
+    <!-- Icone nas telas -->
+    <link rel="icon" href="{{ url('img/icon.webp') }}">
+    <!-- Bootstrap modificavel -->
+    <link rel="stylesheet" href="{{ asset('site/style.css') }}">
 
-  <title>@yield('title')</title>
-  <!-- Não esta funcionanodo/ icone nas telas -->
-  <link rel="shortcut icon"   href="{{ url('css/icon.webp') }}">
-  <!-- Bootstrap modificavel -->
-  <link rel="stylesheet" href="{{ asset('site/style.css') }}">
+    <!-- Jquery -->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
 
-  <!-- Jquery -->
-  <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-
+    {{-- FontAwsome/ Icons --}}
+    <script src="https://kit.fontawesome.com/b1621cc4d4.js" crossorigin="anonymous"></script>
 </head>
 
-    <body>
-    <h3>Layouts/App</h3>
+<body>
+  <div class="">
+    @yield('content')
+    <nav class="navbar navbar-expand-lg navbar-dark bg-gradient-primary ">
+      
+    </nav>
+  </div>
 
     <script src="{{ asset('site/jquery.js') }}"></script>
     <script src="{{ asset('site/bootstrap.js') }}"></script>
-    </body>
+</body>
+
 </html>
