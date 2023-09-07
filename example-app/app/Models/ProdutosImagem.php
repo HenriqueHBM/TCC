983684a19cfe9e07ca8ebcd9d240f09c7b854b86
HenriqueHBM <henriquemadeira1@gmail.com>
2023-09-07 13:54:44 -0300

    triste

--- example-app/app/Models/ProdutosImagem.php
+++ example-app/app/Models/ProdutosImagem.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class ProdutosImagem extends Model
 {
     use HasFactory;
-    protected $table = 'produtos_imagens';
+    protected $table = 'imagens_produtos';
 }
